80bf376302bb27b16c8346d7f5ca02a9355f1895
GianbaBotassi <gianba89@gmail.com>
2023-07-11 15:43:37 +0200

    Add repository, layout and mixins/variables

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+  <h1>sono footer blade php</h1>
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,18 @@
+<header class="container">
+  <div>
+      <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="Logo">
+  </div>
+  <ul>
+    <li>CHARACTERS</li>
+    <li>COMICS</li>
+    <li>MOVIES</li>
+    <li>TV</li>
+    <li>GAMES</li>
+    <li>COLLECTIBLES</li>
+    <li>VIDEOS</li>
+    <li>FANS</li>
+    <li>NEWS</li>
+    <li>SHOP</li>
+  </ul>
+</header>
+

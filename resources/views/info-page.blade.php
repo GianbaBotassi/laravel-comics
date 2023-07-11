ccbe49bb5bd2887a832ed302623211d002fdf378
GianbaBotassi <gianba89@gmail.com>
2023-07-11 17:13:57 +0200

    put db cards before routes in web.php, the recall it into function with use()

--- resources/views/info-page.blade.php
+++ resources/views/info-page.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.layout')
 
 @section('content')
-<main>
+<main id="info-page">
   <div class="container">
-    <span id="current-series">inserire img card</span>
+    {{-- <span id="current-series">inserire img card</span> --}}
+    
     <div>
-      
-      <div>
-        
-      </div> 
+      <h4>{{$cards[0]['title']}}</h4>
+      <p>{{$cards[0]['description']}}</p>
+    </div>
+    <div>ciao</div>
 </div>
 </main>
 @endsection
